0a28a60b8706029a52f206de2762f5f38ff63bf7
carlos-sardina <developerAlbertoMT@gmail.com>
2023-12-28 21:33:47 -0600

    jest setup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,13 +6,13 @@ type ButtonProps = {
   type?: 'button' | 'submit' | 'reset';
 };
 
-export const Button = ({ text, icon, isInactive = false, onClick = () => null, type = 'button' }: ButtonProps) => {
+export const Button = ({ text, icon, isInactive = false, onClick, type = 'button' }: ButtonProps) => {
   return (
     <button
       className={`flex items-center gap-1 bg-purpleStrong text-white hover:bg-purpleStronger py-2 px-3 rounded-md ${
         isInactive ? 'opacity-60' : ''
       }`}
-      onClick={onClick}
+      onClick={onClick && onClick}
       type={type}
     >
       {text} {icon}
