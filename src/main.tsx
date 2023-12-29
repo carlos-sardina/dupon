03b426489b3bd63f19fd8115a6e07eee5dd0c254
carlos-sardina <developerAlbertoMT@gmail.com>
2023-12-28 18:48:40 -0600

    adding toastify

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './main.css';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { appStore } from './store/index';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={appStore}>
       <App />
+      <ToastContainer />
     </Provider>
   </React.StrictMode>,
 );
